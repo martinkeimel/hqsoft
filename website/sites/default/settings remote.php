981adf964c781386f46a1ee4a9c5993ba6155913
martinkeimel <martinkeimel@gmail.com>
2016-03-07 15:40:29 -0300

    settings

--- website/sites/default/settings remote.php
+++ website/sites/default/settings remote.php
@@ -246,7 +246,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '0AWvhiQVeLZFD_D3q3Sgkr92BizD9ROc2w_VGepcPjw';
+$drupal_hash_salt = '*';
 
 /**
  * Base URL (optional).
@@ -492,7 +492,7 @@ ini_set('session.cookie_lifetime', 2000000);
  *
  * Add leading hash signs if you would like to disable this functionality.
  */
-$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
+$conf['404_fast_paths_exclude'] = '/\/(?:styles)|(?:system\/files)\//';
 $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
